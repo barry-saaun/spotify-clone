62b5a4a15d02fd179c46fcc8a15c6eef89c7986c
Saaun Sovannpanha <panhaocr@gmail.com>
2023-06-27 10:15:00 +0700

    added a function where it greets base on client's local time

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function currentTime(): string {
+  const hour:number = new Date().getHours()
+  let greeting = ''
+  if (hour > 6 && hour < 12) {
+    greeting = "Morning";
+    return greeting;
+  } else if (hour >= 12 && hour < 18) {
+    greeting = "Afternoon";
+    return greeting;
+  } else if ((hour > 19 && hour < 24) || (hour > 0 && hour < 6)) {
+    greeting = "Evening";
+    return greeting;
+  }
+  return greeting;
+}
+
